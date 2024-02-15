3224cb6e2389f46af3ded9eb80a7bb969bd0141f
Michael Cleary <michaelcleary@MMacBookPro.attlocal.net>
2024-02-15 10:28:24 -0600

    update useUrlProps hook

--- app/playground/@propsController/heading/page.tsx
+++ app/playground/@propsController/heading/page.tsx
@@ -1,26 +1,13 @@
 "use client"
 
-import { useCallback } from "react"
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import Heading, { HeadingDefinition } from "@/components/Heading"
 import Input from "@/components/Input"
 import useUrlProps from "@/hooks/useUrlProps"
 
 const HeadingDisplay = () => {
   const searchParams = useSearchParams()
-  const pathname = usePathname()
-  const router = useRouter()
-  const props = useUrlProps(HeadingDefinition)
-
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString())
-      params.set(name, value)
-
-      return params.toString()
-    },
-    [searchParams]
-  )
+  const [props, updateProp] = useUrlProps(HeadingDefinition)
 
   return (
     <>
@@ -34,7 +21,7 @@ const HeadingDisplay = () => {
               label={prop}
               labelClassName="text-xl text-right"
               value={searchParams.get(prop) as string}
-              onChange={(e) => router.replace(`${pathname}?${createQueryString(prop, e.target.value)}`)}
+              onChange={(e) => updateProp(prop, e.target.value)}
             />
           ))
         }
--- hooks/useUrlProps.tsx
+++ hooks/useUrlProps.tsx
@@ -1,15 +1,30 @@
+import { useCallback } from "react"
+import { usePathname, useRouter } from "next/navigation"
 import { ComponentDefinition } from "@/components/Heading"
 import { useSearchParams } from "next/navigation"
 
 const useUrlProps = (componentDefinition: ComponentDefinition) => {
+  const router = useRouter()
+  const pathname = usePathname()
   const searchParams = useSearchParams()
 
-  const props = Object.entries(componentDefinition).reduce((acc, [prop, val]) => {
+  const updateQueryString = useCallback((name: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString())
+    params.set(name, value)
+
+    return params.toString()
+  }, [searchParams])
+
+  const updateUrlProp = useCallback((prop: string, value: string) => {
+    return router.replace(`${pathname}?${updateQueryString(prop, value)}`)
+  }, [pathname])
+
+  const urlProps = Object.entries(componentDefinition).reduce((acc, [prop, val]) => {
     if (prop !== 'className') acc[prop] = searchParams.get(prop) || val.default
     return acc
   }, {} as Record<string, any>)
 
-  return props
+  return [urlProps, updateUrlProp] as const
 }
 
 export default useUrlProps
